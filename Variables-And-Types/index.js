3e722acfe9c436c786cd7418287d350b9deccfb3
Shachar Shapira <shapi78@gmail.com>
2022-08-31 02:48:30 +0300

    Fixing Types

---
+++ Variables-And-Types/index.js
@@ -0,0 +1,30 @@
+// let name = 'Baba';
+// Cannot be reserved keyword
+// Should be meaningful
+// Cannot start with a number
+// Cannot contain space or hyphen
+// Camel Notation case-sensitive
+
+// Primitive Variables:
+let name = "Shaul"; //String Literal
+let age = 12; // Numebr Literal
+let x = 10;
+let y = 12;
+let isDone = true; //Boolean
+let yoyo= undefined; //undefined
+let selectedColor = null; // null - clear value
+let firstName = 'Shapi', lastName = 'Bulele';
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = 'brown';
+selectedColors[4] = 1;
+selectedColors[9] = 'white';
+name = "Kukia";
+typeof name;
+
+// object
+let person = {
+    name: 'Mike',
+    age: 27
+}; //object literal
+
+
